8fe00573deb47a71d6e62f09b962ebb65ed5b2b9
IsaoTakahashi <ricto415@gmail.com>
2015-06-13 16:56:56 +0900

    create project

---
+++ CountUp/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  CountUp
+//
+//  Created by 高橋 勲 on 2015/06/13.
+//  Copyright © 2015年 高橋 勲. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var countButton: UIButton!
+    
+    var tappedCount :Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        displayTappedCount()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func displayTappedCount() {
+        countLabel.text = String(tappedCount)
+    }
+
+    @IBAction func clickButton(sender: AnyObject) {
+        tappedCount++;
+        displayTappedCount()
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# カウントアップアプリ
+Xcode7のUI recordingテスト用アプリ
+
+## Reference
+- [Qiitaのページ](http://qiita.com/hogehoge)
+
